7a4c97e3f327b63fa46f576142b6fcb1d1068acd
Raj Vengala <rvengala@wrench.com>
2021-06-12 20:10:28 +0530

    init

---
+++ lib/src/args.dart
@@ -0,0 +1,53 @@
+import 'package:args/args.dart';
+
+class Args {
+  static ArgParser parser() {
+    ArgParser parser = ArgParser();
+
+    parser.addOption(
+      'screen',
+      abbr: 's',
+      help:
+          'Creates screen files and a model in lib/src/screens and lib/src/models directories respectively\n'
+          ' lib/src/screens/booking/new_user/new_user_service.dart\n'
+          ' lib/src/screens/booking/new_user/new_user_state.dart\n'
+          ' lib/src/screens/booking/new_user/new_user_screen.dart\n'
+          ' lib/src/models/new_user.dart\n',
+      valueHelp: 'booking/new_user',
+    );
+
+    parser.addOption(
+      'model',
+      abbr: 'm',
+      help: 'Creates a model in lib/src/models directory\n'
+          ' lib/src/models/vehicle.dart\n',
+      valueHelp: 'vehicle',
+    );
+
+    parser.addFlag(
+      'build',
+      abbr: 'b',
+      help: 'Generates runtime files',
+      defaultsTo: false,
+      negatable: false,
+    );
+
+    parser.addFlag(
+      'clean',
+      abbr: 'd',
+      help: 'Deletes runtime files',
+      defaultsTo: false,
+      negatable: false,
+    );
+
+    parser.addFlag(
+      'watch',
+      abbr: 'w',
+      help: 'Monitors files for changes and re-generate runtime files',
+      defaultsTo: false,
+      negatable: false,
+    );
+
+    return parser;
+  }
+}
